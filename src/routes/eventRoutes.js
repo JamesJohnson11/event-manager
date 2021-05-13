65edf3d2b36b00687f50029e38abb0505f970f3a
Barry Johnson <bdotmgmt@gmail.com>
2021-05-13 02:03:04 -0400

    Create route to retrieve all events

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const evCtrl = require('../controllers/eventControllers');
 
 // POST request to /events to create new event
-router.post('/events', evCtrl.createNewEvent)
+router.post('/events', evCtrl.createNewEvent);
 
-
+// GET request to /events to fetch all events
+router.get('/events', evCtrl.fetchEvents);
 
 module.exports = router;
