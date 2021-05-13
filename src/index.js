05512e2c25388c03f52bfc4c338a2e6415a44fef
Barry Johnson <bdotmgmt@gmail.com>
2021-05-12 23:20:04 -0400

    Add database setup to entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const dbSetup = require('./database/dbSetup');
 
+
+dbSetup();
 
 app.listen(port, () => {
     console.log(`Server is now running on port ${port}`);
