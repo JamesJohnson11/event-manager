77b712b531acd3de151e6c0755f496f8ad990329
Barry Johnson <bdotmgmt@gmail.com>
2021-05-13 01:59:11 -0400

    Use event routes in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const dbSetup = require('./database/dbSetup');
+const eventRoutes = require('./routes/eventRoutes');
+
+app.use(express.json());
 
 
 dbSetup();
+
+app.use(eventRoutes);
 
 app.listen(port, () => {
     console.log(`Server is now running on port ${port}`);
