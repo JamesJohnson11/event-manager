a61698af5c9147a64a51e3a91e99dadcb62c7637
Barry Johnson <bdotmgmt@gmail.com>
2021-05-22 22:32:36 -0400

    Add authentication for all evvent routes and admin authentication for create, update, and delete event routes

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const evCtrl = require('../controllers/eventControllers');
+const { authenticateUser, checkIfAdmin } = require('../middlewares/authentication');
 
 // POST request to /events to create new event
-router.post('/events', evCtrl.createNewEvent);
+router.post('/events', authenticateUser, checkIfAdmin, evCtrl.createNewEvent);
 
 // GET request to /events to fetch all events
-router.get('/events', evCtrl.fetchEvents);
+router.get('/events', authenticateUser, evCtrl.fetchEvents);
 
 // GET request to /events/:id to fetch a single event
-router.get('/events/:id', evCtrl.fetchSingleEvent);
+router.get('/events/:id', authenticateUser, evCtrl.fetchSingleEvent);
 
 // PUT request to /events/:id to update a single event
-router.put('/events/:id', evCtrl.updateSingleEvent);
+router.put('/events/:id', authenticateUser, checkIfAdmin, evCtrl.updateSingleEvent);
 
 // DELETE request to /events/:id to delete a single event
-router.delete('/events/:id', evCtrl.deleteSingleEvent);
+router.delete('/events/:id', authenticateUser, checkIfAdmin, evCtrl.deleteSingleEvent);
 
 module.exports = router;
