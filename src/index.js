bcd90224b4cddf6b44e24cfe81a03c419aaa4fab
Barry Johnson <bdotmgmt@gmail.com>
2021-05-22 22:16:26 -0400

    Require admin seeder in index.js file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const dbSetup = require('./database/dbSetup');
+
+// REQUIRE ROUTES
 const eventRoutes = require('./routes/eventRoutes');
 const authRoutes = require('./routes/authRoutes');
 
+
+// SEEDERS
+const { seedAdmin } = require('./seeders/admin');
 
 require('dotenv').config();
 
 
 app.use(express.json());
 
-
+// SETUP DB
 dbSetup();
 
 app.use(eventRoutes);
